b5e021b389f6940d495994dbb7cdbe21e8e5f8d3
ridwanmaelfa <ridwan.maelfa@ifabula.com>
2021-12-14 12:31:32 +0700

    handle icon from server path

--- src/components/Homepage/HPSection2Fix.js
+++ src/components/Homepage/HPSection2Fix.js
@@ -1,68 +1,174 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import iconCar from '../../assets/img/IconCar.png'
-import iconMotor from '../../assets/img/iconMotor.png'
-import iconRental from '../../assets/img/iconRental.png'
-import iconAsuransi from '../../assets/img/iconAsuransi.png'
-import iconRp from '../../assets/img/iconRp.png'
-import iconMasjid from '../../assets/img/iconMasjid.png'
-import iconTractor from '../../assets/img/iconTractor.png'
-import iconElectric from '../../assets/img/iconElectric.png'
-import iconTabungan from '../../assets/img/iconTabungan.png'
-
+import iconCar from "../../assets/img/IconCar.png";
+import iconMotor from "../../assets/img/iconMotor.png";
+import iconRental from "../../assets/img/iconRental.png";
+import iconAsuransi from "../../assets/img/iconAsuransi.png";
+import iconRp from "../../assets/img/iconRp.png";
+import iconMasjid from "../../assets/img/iconMasjid.png";
+import iconTractor from "../../assets/img/iconTractor.png";
+import iconElectric from "../../assets/img/iconElectric.png";
+import iconTabungan from "../../assets/img/iconTabungan.png";
 
 const HPSection2Fix = (props) => {
-  
-  const [products,setProducts] = useState([
-    {image:iconCar, text:'Pembiayaan Mobil',link:'/product/car'},
-    {image:iconMotor, text:'Pembiayaan Motor',link:'/product/motor'},
-    {image:iconRental, text:'Rental',link:'/product/rental'},
-    {image:iconAsuransi, text:'Asuransi Kesehatan',link:'/product/asuransi'},
-    {image:iconRp, text:'Pinjaman Tunai',link:'/product/pinjaman'},
-    {image:iconMasjid, text:'Perjalanan Religi',link:'/product/perjalanan'},
-    {image:iconTractor, text:'Truk dan Alat Berat',link:'/product/alatberat'},
-    {image:iconElectric, text:'Elektronik dan Lainnya',link:'/product/elektronik'},
-    {image:iconTabungan, text:'Tabungan',link:'/product/tabungan'}
-  ])
-        const productsData = props.products.length>0? props.products.map((product,i)=>
-            i==5?
-              <div className="col HPSection2" style={{marginBottom:'30px',marginLeft:'120px'}}>
-              <a href={`/product/detail/${product.slug}`} style={{cursor:'pointer',textDecoration:'none'}}>
-              <img height="128px" width="128px" src={product.icon.url} style={{marginBottom:'15px'}}/><br/>
+  const [products, setProducts] = useState([
+    { image: iconCar, text: "Pembiayaan Mobil", link: "/product/car" },
+    { image: iconMotor, text: "Pembiayaan Motor", link: "/product/motor" },
+    { image: iconRental, text: "Rental", link: "/product/rental" },
+    {
+      image: iconAsuransi,
+      text: "Asuransi Kesehatan",
+      link: "/product/asuransi",
+    },
+    { image: iconRp, text: "Pinjaman Tunai", link: "/product/pinjaman" },
+    {
+      image: iconMasjid,
+      text: "Perjalanan Religi",
+      link: "/product/perjalanan",
+    },
+    {
+      image: iconTractor,
+      text: "Truk dan Alat Berat",
+      link: "/product/alatberat",
+    },
+    {
+      image: iconElectric,
+      text: "Elektronik dan Lainnya",
+      link: "/product/elektronik",
+    },
+    { image: iconTabungan, text: "Tabungan", link: "/product/tabungan" },
+  ]);
+  const productsData =
+    props.products.length > 0 ? (
+      props.products.map((product, i) =>
+        i == 5 ? (
+          <div
+            className="col HPSection2"
+            style={{ marginBottom: "30px", marginLeft: "120px" }}
+          >
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={product.icon.url}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
               <h4 className="productsWording">{product.name}</h4>
-              </a>
-              </div>
-            : i==8?
-              <div className="col HPSection2" style={{marginBottom:'30px',marginRight:'120px'}}>
-              <a href={`/product/detail/${product.slug}`} style={{cursor:'pointer',textDecoration:'none'}}>
-              <img height="128px" width="128px" src={product.icon.url} style={{marginBottom:'15px'}}/><br/>
+            </a>
+          </div>
+        ) : i == 5 && product.icon.url.match(/uploads/) ? (
+          <div
+            className="col HPSection2"
+            style={{ marginBottom: "30px", marginLeft: "120px" }}
+          >
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={`https://dev.moxa.id/cms/${product.icon.url}`}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
               <h4 className="productsWording">{product.name}</h4>
-              </a>
-              </div>
-            :
-            <div className="col HPSection2" style={{marginBottom:'30px'}}>
-              <a href={`/product/detail/${product.slug}`} style={{cursor:'pointer',textDecoration:'none'}}>
-              <img height="128px" width="128px" src={product.icon.url} style={{marginBottom:'15px'}}/><br/>
+            </a>
+          </div>
+        ) : i == 8 ? (
+          <div
+            className="col HPSection2"
+            style={{ marginBottom: "30px", marginRight: "120px" }}
+          >
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={product.icon.url}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
               <h4 className="productsWording">{product.name}</h4>
-              </a>
-            </div>
-           
-       ): <h2>loading..</h2>
-  
+            </a>
+          </div>
+        ) : i == 8 && product.icon.url.match(/uploads/) ? (
+          <div
+            className="col HPSection2"
+            style={{ marginBottom: "30px", marginRight: "120px" }}
+          >
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={`https://dev.moxa.id/cms/${product.icon.url}`}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
+              <h4 className="productsWording">{product.name}</h4>
+            </a>
+          </div>
+        ) : product.icon.url.match(/uploads/) ? (
+          <div className="col HPSection2" style={{ marginBottom: "30px" }}>
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={`https://dev.moxa.id/cms/${product.icon.url}`}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
+              <h4 className="productsWording">{product.name}</h4>
+            </a>
+          </div>
+        ) : (
+          <div className="col HPSection2" style={{ marginBottom: "30px" }}>
+            <a
+              href={`/product/detail/${product.slug}`}
+              style={{ cursor: "pointer", textDecoration: "none" }}
+            >
+              <img
+                height="128px"
+                width="128px"
+                src={product.icon.url}
+                style={{ marginBottom: "15px" }}
+              />
+              <br />
+              <h4 className="productsWording">{product.name}</h4>
+            </a>
+          </div>
+        )
+      )
+    ) : (
+      <h2>loading..</h2>
+    );
+
   return (
     <div>
       <div id="sub">
         <div className="wrapper">
-          <div className="row" style={{marginBottom:'35px'}}>
+          <div className="row" style={{ marginBottom: "35px" }}>
             <div className="col-lg-10 m-auto">
-              <h1 >Jelajahi Berbagai Pilihan Produk Moxa </h1>
-              <p >Rasakan kemudahan mengajukan beragam produk keuangan dalam satu sentuhan lewat fitur unggulan Moxa </p>
+              <h1>Jelajahi Berbagai Pilihan Produk Moxa </h1>
+              <p>
+                Rasakan kemudahan mengajukan beragam produk keuangan dalam satu
+                sentuhan lewat fitur unggulan Moxa{" "}
+              </p>
             </div>
           </div>
-          <div className="row HPSection2-wrapper" >
-          {productsData}
-          </div>
-         
+          <div className="row HPSection2-wrapper">{productsData}</div>
         </div>
       </div>
     </div>
